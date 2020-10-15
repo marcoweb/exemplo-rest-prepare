dd6de2694aae5a53be29433d7f21d84bfe28c7fc
marcoweb <marcocarvalho.web@hotmail.com>
2020-10-15 12:45:17 +0000

    Init

--- app.js
+++ app.js
@@ -2,8 +2,47 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.json())
+
+
+
+const tarefas = [
+    {id: 1, descrição : "Ler livro", concluída: false},
+    {id: 2, descrição : "Escrever Resumo", concluída: false}
+];
+
 app.use(express.static('public'));
 
+app.get("/tarefas", function(req, res) {
+    res.json(tarefas);
+});
+
+app.get("/tarefas/:id", function(req, res) {
+    const { id } = req.params;
+    const tarefa = tarefas.find(t => t.id == id);
+    res.json(tarefa);
+});
+
+app.put("/tarefas", function(req, res) {
+    const {id, descrição, concluída} = req.body;
+    tarefa = tarefas.find(t => t.id == id);
+    tarefa.descrição = descrição;
+    tarefa.concluída = concluída;
+    res.json(tarefa);
+});
+
+app.post("/tarefas", function(req, res) {
+    const tarefa = req.body;
+    tarefas.push(tarefa);
+    res.json(tarefa);
+});
+
+app.delete("/tarefas", function(req, res) {
+    const {id, descrição, concluída} = req.body;
+    tarefa = tarefas.find(t => t.id == id);
+    tarefas.splice(tarefas.indexOf(t => t.id == tarefa.id), 1);
+    res.json(tarefa);
+});
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
---
+++ public/js/index.js
@@ -0,0 +1,65 @@
+var tarefas =  [];
+
+function addTarefa() {
+    let tarefa = {
+        id : tarefas[tarefas.length -1].id + 1,
+        descrição : document.querySelector("#txtDescricao").value,
+        concluída : false
+    }
+    axios.post(window.location.href + "tarefas", tarefa).then(function(response) {
+        getTarefas();
+    });
+    document.querySelector("#txtDescricao").value = "";
+}
+
+function removeTarefa(id) {
+    tarefa = tarefas.find(t => t.id == id);
+    axios.delete(window.location.href + "tarefas", tarefa).then(function(response) {
+        getTarefas();
+    });
+}
+
+function setConcluida(id) {
+    tarefa = tarefas.find(t => t.id == id);
+    tarefa.concluída = !tarefa.concluída;
+    axios.put(window.location.href + "tarefas", tarefa).then(function(response) {
+        getTarefas();
+    });
+}
+
+function getTarefas() {
+    axios.get(window.location.href + "tarefas").then(function(response) {
+        tarefas = [];
+        tarefas = response.data;
+        carregaTarefas();
+    });
+}
+
+function carregaTarefas() {
+    let tabela = document.querySelector("#tbl_tarefas");
+    tabela.innerHTML = '';
+    tarefas.forEach(function(t) {
+        let linha = document.createElement('tr');
+        let coluna_desc = document.createElement('td');
+        coluna_desc.innerHTML = t.descrição;
+        linha.appendChild(coluna_desc);
+        let coluna_conc = document.createElement('td');
+        let btn_concluido = document.createElement('button');
+        btn_concluido.setAttribute('type', 'button');
+        btn_concluido.setAttribute('onclick', 'setConcluida(' + t.id + ')');
+        btn_concluido.innerHTML = (t.concluída) ? "SIM" : "NÃO";
+        coluna_conc.appendChild(btn_concluido);
+        let btn_remove = document.createElement('button');
+        btn_remove.setAttribute('type', 'button');
+        btn_remove.setAttribute('onclick', 'removeTarefa(' + t.id + ')');
+        btn_remove.innerHTML = 'Remover Ítem';
+        coluna_conc.appendChild(btn_remove);
+
+        linha.appendChild(coluna_conc);
+
+        tabela.appendChild(linha);
+    });
+}
+
+
+getTarefas();
